11806fea31975983b9661e92ea0d44c974250990
Maxim <maxim_zykov@inbox.ru>
2020-09-07 21:15:02 +0300

    Lab 1

---
+++ lab1-Zykov/src/ru/mirea/lab1/Lab1.java
@@ -0,0 +1,17 @@
+package ru.mirea.lab1;
+
+public class Lab1 {
+    public static void main(String[] args) {
+        for (int i=1;i<11;i++){
+            if(i==1)
+            {
+                System.out.print(i);
+            }
+            else
+            {
+                System.out.print(", 1/"+i);
+            }
+
+        }
+    }
+}
